1f2a5dbffff98e98e52831f806ba36bbcd62ecc7
Ronald Huereca <ronald@mediaron.com>
2022-09-07 12:02:42 -0500

    Adding alert build scripts.

--- build/alerts-dlx.asset.php
+++ build/alerts-dlx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0aab4708d3452362aedc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '02e0a6683a334b527687');
