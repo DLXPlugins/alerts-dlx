1ce42154c6338f8475ff79f1201b9fa2da359195
Ronald Huereca <ronald@mediaron.com>
2022-09-07 11:49:56 -0500

    Updating grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,12 @@ module.exports = function (grunt) {
 		compress: {
 			main: {
 			  options: {
-				archive: 'quotes-dlx.zip'
+				archive: 'alerts-dlx.zip'
 			  },
 			  files: [
-				{src: ['quotes-dlx.php'], dest: '/', filter: 'isFile'}, // includes files in path
-				{src: ['readme.md'], dest: '/', filter: 'isFile'}, // includes files in path
+				{src: ['alerts-dlx.php'], dest: '/', filter: 'isFile'}, // includes files in path
 				{src: ['readme.txt'], dest: '/', filter: 'isFile'},
 				{src: ['php/**'], dest: '/'}, // includes files in path and its subdirs
-				{src: ['images/**'], dest: '/'},
 				{src: ['lib/**'], dest: '/'}, // includes files in path and its subdirs
 				{src: ['dist/**'], dest: '/'}, // includes files in path and its subdirs
 				{src: ['build/**'], dest: '/'}, // includes files in path and its subdirs
