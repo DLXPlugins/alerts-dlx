c3cb6d9533e72ca8dbfe4f61c6d1972f4c877815
Ronald Huereca <ronald@mediaron.com>
2022-09-12 08:37:05 -0500

    Auto stash before merge of "main" and "origin/main"

--- build/alerts-dlx.asset.php
+++ build/alerts-dlx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1b06ff85f718bd59a6c2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '697787272b79beece2cb');
