bf7635d48b4ba25904e838d96766d508593b6271
Ronald Huereca <ronald@mediaron.com>
2022-09-12 08:51:23 -0500

    Updating SVG icons.

--- build/alerts-dlx.asset.php
+++ build/alerts-dlx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1b06ff85f718bd59a6c2');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0060cc2f6a0b9feacaf1');
