e5ae5a34a2c84f370ed43ea0d1535ce51721e42c
Ronald Huereca <ronald@mediaron.com>
2022-09-12 08:51:57 -0500

    Doing prod build for release.

--- build/alerts-dlx.asset.php
+++ build/alerts-dlx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0060cc2f6a0b9feacaf1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '471c20e8b2c10c2ce847');
