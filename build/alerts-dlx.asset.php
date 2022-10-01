6908439388cdc62b02384e4304f5cf89c943cf66
Ronald Huereca <ronald@mediaron.com>
2022-10-01 17:03:29 -0500

    Doing prod build.

--- build/alerts-dlx.asset.php
+++ build/alerts-dlx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '781642a16d01d0301c80');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2e196a0379c294e9a965');
