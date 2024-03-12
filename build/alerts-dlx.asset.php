37f0cc756d3a3ed30196bb2daf761bc551a8a1ee
Ronald Huereca <ronald@mediaron.com>
2024-03-12 02:25:39 -0500

    Testing new version.

--- build/alerts-dlx.asset.php
+++ build/alerts-dlx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '62ce7e29d69d7f04c37f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6b3be09433df70b51a62');
--- src/js/blocks/bootstrap/edit.js
+++ src/js/blocks/bootstrap/edit.js
@@ -268,7 +268,7 @@ const BootstrapAlerts = ( props ) => {
 	const maxWidthStyle = {
 		maxWidth: maximumWidth + maximumWidthUnit,
 	};
-	const baseFontSizeStyles = `#${ uniqueId } { font-size: ${ parseInt( baseFontSize ) }px; }`;
+	const baseFontSizeStyles = `#${ uniqueId } { font-size: ${ parseInt( baseFontSize ) }px; }#${ uniqueId }  figcaption { --theme-font-size: ${ parseInt( baseFontSize ) }px; }`;
 	const block = (
 		<>
 			<InspectorControls>{ inspectorControls }</InspectorControls>
